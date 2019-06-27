5cb1c4699d0c92a71c2ad1e80bedeb665b90f6b2
Rob Widdick <rob@36creative.com>
2019-06-27 13:05:11 -0400

    Update readme, cleanup and document code, add screenshots for wp plugin page

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 === Plugin Name ===
 Contributors: robertark
 Donate link: https://robwiddick.com
-Tags: acf, flexible content, preview
+Tags: acf, flexible content, preview layout
 Requires at least: 4.0
 Tested up to: 5.2.2
 Stable tag: 5.2.2
@@ -12,10 +12,9 @@ See an example of the ACF Flexible Content Layout before you select it!
 
 == Description ==
 
-Note: This requires ACF Pro.
-
 This plugin allows you to upload image examples to your uploads directory so that your clients (or users) can see
-an example of what the ACF Flexible Content Layout looks like before they select it.
+an example of what the ACF Flexible Content Layout looks like in the page editor before they select it. This modifies
+the existing tooltip to display an image.
 
 Be sure to review the INSTALLATION guide!
 
@@ -25,9 +24,11 @@ To install:
 
 1. Upload `acf-fc-layout-previews` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Upload JPG images of your flexible content layouts into your uploads/acf-fc-layouts directory. The name of the image should be the slug of the template name.
+3. Upload JPG images of your flexible content layouts into your uploads/acf-fc-layouts directory.
 
-For example, if you have a layout "Hero", then you must upload "hero.jpg" into uploads/acf-fc-layouts.
+NOTE: The name of the image should be the name of the flexible layout with the ".jpg" extension.
+
+For example, if you have a layout "Hero", then you must upload "hero.jpg" into _upload-dir_/acf-fc-layouts.
 
 Another example, if you have a layout titled "Content row with side image", then you must name the image "content-row-with-side-image.jpg"
 
@@ -39,7 +40,15 @@ Currently, only jpeg. The file extension must be "jpg" - all lowercase. Example:
 
 = Why isn't this more dynamic? =
 
-I don't have the time. It's on my todo list, but feel free to contribute!
+I don't have the time. It's on my todo list; feel free to contribute!
+
+= I have a question =
+
+Please submit a support post.
+
+= I want to contribute =
+
+Awesome! Fork the git repo here https://github.com/robertark/ACF-Flexible-Content-Layout-Previews and submit a pull request when ready!
 
 == Screenshots ==
 
--- admin/js/acf-fc-layout-previews-admin.js
+++ admin/js/acf-fc-layout-previews-admin.js
@@ -5,39 +5,62 @@
 (function( $ ) {
 	'use strict';
 
+	// Get the current uploads directory
 	let preview_path = window.acf_fc_layout_previews_uploads_url;
-	let el = '.acf-fc-popup';
-	let hasImg = false;
 
+	// Define the tooltip class that appears from the layout selection buttons
+	let el = '.acf-fc-popup';
+
+	// Other vars
+	let hasImg = false;
+	let intPopupCheck = null;
+
+	// Wait for the tooltip to appear in the DOM. We are using jQuery Initialize for our Mutation Observer. It's just easier that way for me.
 	$.initialize(el, function() {
 		$("ul li", el).each(function () {
-			var $self = $(this);
-			var $list_item = $self.find("a");
-			var layout = $list_item.data("layout");
+			let $self = $(this);
+			let $list_item = $self.find("a");
+			let layout = $list_item.data("layout");
+			let img_url = preview_path + layout + '.jpg';
+			let layout_name = $list_item.text();
 
-			var img_url = preview_path + layout + '.png';
-			var layout_name = $list_item.text();
+			// Attempt to download the preview image. This allows us to test if the image exists or not.
 			$.get(img_url).done(function() {
 				hasImg = true;
+				// Append the image. This is rough and could be refactored, but it works for now.
 				$list_item.html(layout_name + "<br><img src='" + img_url + "'>");
 			}).fail(function () {
+				// We do this check in the case of if any image exists, then all non-existent areas will show an error.
+				// If there are no images, we don't change the layout selection tooltip
 				if (hasImg) {
-					$list_item.html(layout_name + "<hr>");
+					// Again, I'm sure this could be refactored
+					$list_item.html(layout_name + "<br><em>(Example preview not available, image <abbr title='" + img_url + "'>" + layout + ".jpg</abbr> missing.)</em><hr>");
 				}
 			});
 		});
 	});
 
-	let intPopupCheck = null;
+	// Here, we check if any image exists for layout previews. If so, we display the background and modify the tooltip container.
 	$.initialize(el + " ul li img", function() {
 		if(!$(el).find(".wrapper").length) {
+			// Remove forced positioning applied to tooltip and add our class
 			$(el).removeAttr("style").addClass('acf-admin-popup-enabled');
+
+			// Wrap the ul in a container. This was done because when applying a left: 50% / transform: translateX(-50%)
+			// it results in the entire container having blurry content (text, images). I could not get any "fix" to actually
+			// work here. Please feel free to suggest alternatives if necessary.
 			$(el).find("ul").wrap("<div class='wrapper'></div>");
+
+			// On click of the background overlay, remove it. Clicking outside of the tooltip also closes the tooltip,
+			// so we don't need to worry about that.
 			$('<div/>').addClass("acf-admin-popup-background").appendTo("body").on("click", function() { $(this).remove(); });
 
-			// We can't listen to the element as usual because of stopPropagation being used... so we'll be hacky
+			// We can't listen to the element as usual because of stopPropagation being used...
+			// This is the only workaround I have found to work. If you have another idea, please submit it!
 			intPopupCheck = setInterval(function() {
+				// Check if the tooltip no longer exists
 				if(!$(el).length) {
+					// If the tooltip is non-existent, remove the background overlay and stop the interval timer check
 					$(".acf-admin-popup-background").remove();
 					clearInterval(intPopupCheck);
 				}
